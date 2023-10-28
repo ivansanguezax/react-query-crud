3de0ec1b6b6e91e8a8e75bf5a5ca5ce6d2c2a6e9
ivansanguezax <ivansanguezax@gmail.com>
2023-10-28 12:29:26 -0400

    [006-updateProduct]

--- src/api/productsAPI.js
+++ src/api/productsAPI.js
@@ -1,14 +1,16 @@
 import axios from "axios";
 
 const productsAPI = axios.create({
-    baseURL: "http://localhost:3000/products"
+  baseURL: "http://localhost:3000/products",
 });
 
 export const getProducts = async () => {
-    const response = await productsAPI.get("/");
-    return response.data;
-} 
+  const response = await productsAPI.get("/");
+  return response.data;
+};
 
 export const createProduct = async (product) => productsAPI.post("/", product);
 
 export const deleteProduct = async (id) => productsAPI.delete(`/${id}`);
+
+export const updateProduct = (product) => productsAPI.put(`/${product.id}`, product);
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,47 +1,66 @@
 import { useQuery, useMutation, useQueryClient } from "react-query";
-import { getProducts, deleteProduct } from "../api/productsAPI";
-
-
+import { getProducts, deleteProduct, updateProduct } from "../api/productsAPI";
 
 function Products() {
-    const queryClient = useQueryClient();
-    const {isLoading, data:products, isError, error} = useQuery({
-        queryKey: 'products',
-        queryFn: getProducts,
-        select: products => products.sort((a,b) => b.id - a.id)
-    });
+  const queryClient = useQueryClient();
+  const {
+    isLoading,
+    data: products,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: "products",
+    queryFn: getProducts,
+    select: (products) => products.sort((a, b) => b.id - a.id),
+  });
 
-    const deleteMutation = useMutation({
-        mutationFn: deleteProduct,
-        onSuccess: () => {
-            queryClient.invalidateQueries('products');
-        }
-    })
+  const deleteMutation = useMutation({
+    mutationFn: deleteProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries("products");
+    },
+  });
 
+  const updateMutation = useMutation({
+    mutationFn: updateProduct,
+    onSuccess: () => {
+      queryClient.invalidateQueries("products");
+    },
+  });
 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
 
-    if (isLoading) {
-        return <div>Loading...</div>;
-    } else if (isError) {
-        return <div>Error: {error.message}</div>;
-    }
-
-    
-  
-    return products.map((product) => (
-        <div key={product.id}>
-            <h2>{product.name}</h2>
-            <p>{product.description}</p>
-            <p>{product.price}</p>
-            <p>{product.image}</p>
-            <button onClick={() =>{
-                deleteMutation.mutate(product.id);
-            } 
-            }>delete</button>
-            <input type="checkbox" />
-            <label htmlFor="">In Stock</label>
-        </div>
-    ));
+  return products.map((product) => (
+    <div key={product.id}>
+      <h2>{product.name}</h2>
+      <p>{product.description}</p>
+      <p>{product.price}</p>
+      <p>{product.image}</p>
+      <button
+        onClick={() => {
+          deleteMutation.mutate(product.id);
+        }}
+      >
+        delete
+      </button>
+      <input
+        type="checkbox"
+        checked={product.inStock}
+        id={product.id}
+        onChange={(e) => {
+          updateMutation.mutate({
+            ...product,
+            inStock: e.target.checked,
+          });
+        }}
+      />
+      <label htmlFor={product.id}>In Stock</label>
+    </div>
+  ));
 }
 
 export default Products;
